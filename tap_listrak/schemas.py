eda180289776dc6847916717986ea27d55c971aa
Buck Ryan <buck.ryan2@gmail.com>
2017-11-01 22:55:16 -0400

    Get list and message syncing working

---
+++ tap_listrak/schemas.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os
+import singer
+from singer import utils
+
+
+class IDS(object):
+    LISTS = "lists"
+    MESSAGES = "messages"
+
+stream_ids = [getattr(IDS, x) for x in dir(IDS)
+              if not x.startswith("__")]
+
+PK_FIELDS = {
+    IDS.LISTS: ["ListID"],
+    IDS.MESSAGES: ["MsgID"],
+}
+
+
+def get_abs_path(path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
+
+def load_schema(tap_stream_id):
+    path = "schemas/{}.json".format(tap_stream_id)
+    return utils.load_json(get_abs_path(path))
+
+
+def load_and_write_schema(tap_stream_id):
+    schema = load_schema(tap_stream_id)
+    singer.write_schema(tap_stream_id, schema, PK_FIELDS[tap_stream_id])
--- tap_listrak/streams.py
+++ tap_listrak/streams.py
@@ -1,71 +1,105 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date, timezone
+import pendulum
+from zeep.helpers import serialize_object
 import singer
-from singer import metrics
-from singer.transform import transform as tform
+from singer.utils import strftime
+from . import schemas
+from .schemas import IDS
 
 LOGGER = singer.get_logger()
 
 
-class Stream(object):
-    """Information about and functions for syncing streams.
-
-    Important class properties:
-
-    :var tap_stream_id:
-    :var pk_fields: A list of primary key fields"""
-    def __init__(self, tap_stream_id, pk_fields):
-        self.tap_stream_id = tap_stream_id
-        self.pk_fields = pk_fields
-
-    def metrics(self, records):
-        with metrics.record_counter(self.tap_stream_id) as counter:
-            counter.increment(len(records))
-
-    def write_records(self, records):
-        singer.write_records(self.tap_stream_id, records)
-        self.metrics(records)
-
-    def transform(self, ctx, records):
-        ret = []
-        for record in records:
-            ret.append(tform(record, ctx.schema_dicts[self.tap_stream_id]))
-        return ret
+def gen_intervals(ctx, start_dt):
+    now = datetime.utcnow()
+    interval = timedelta(days=ctx.config.get("interval_days", 60))
+    while start_dt < now:
+        end_dt = min(start_dt + interval, now)
+        yield start_dt, end_dt
+        start_dt = end_dt
 
 
-class SubscribedContacts(Stream):
-    def sync(self, ctx):
-        fn = ctx.client.service.ReportSubscribedContacts
-        for lst in ctx.cache["lists"]:
-            page = 0
-            while True:
-                response = fn(ListID=lst["ListID"], Page=page)
-                records = response["ReportSubscribedContactsResult"]
-                if not records:
-                    break
-                self.write_records(self.transform(ctx, records))
-                page += 1
+def gen_pages():
+    page = 0
+    while True:
+        yield page
+        page += 1
 
 
-class Messages(Stream):
-    def sync(self, ctx):
-        fn = ctx.client.service.ReportListMessageActivity
-        bookmark = [self.tap_stream_id, "SendDate"]
-        start_dt = ctx.update_start_date_bookmark(bookmark)
-        end_dt = datetime.utcnow()
-        for lst in ctx.cache["lists"]:
+def metrics(tap_stream_id, records):
+    with singer.metrics.record_counter(tap_stream_id) as counter:
+        counter.increment(len(records))
+
+
+def write_records(tap_stream_id, records):
+    singer.write_records(tap_stream_id, records)
+    metrics(tap_stream_id, records)
+
+
+def transform_dts(data):
+    if isinstance(data, list):
+        new = []
+        for item in data:
+            new.append(transform_dts(item))
+        return new
+    if isinstance(data, dict):
+        new = {}
+        for k, v in data.items():
+            new[k] = transform_dts(v)
+        return new
+    if isinstance(data, date):
+        new = data.replace(tzinfo=timezone.utc)
+        return strftime(new)
+    return data
+
+
+def transform(response):
+    return transform_dts(serialize_object(response))
+
+
+# class SubscribedContacts(Stream):
+#     def sync(self, ctx):
+#         fn = ctx.client.service.ReportSubscribedContacts
+#         for lst in ctx.cache["lists"]:
+#             for page in gen_pages():
+#                 response = fn(ListID=lst["ListID"], Page=page)
+#                 records = response["ReportSubscribedContactsResult"]
+#                 if not records:
+#                     break
+#                 self.write_records(self.transform(ctx, records))
+
+
+# class MessageClicks(Stream):
+#     def sync(self, ctx, messages):
+#         for msg in messages:
+#             for start_dt, end_dt in gen_intervals(ctx, start_dt):
+#                 for page in gen_pages():
+#                     pass
+#             # ctx.client.service.ReportRangeMessageContactClick(MsgID=msg["MsgID"],
+
+
+# message_clicks = MessageClicks("message_clicks", [""])
+
+
+def sync_messages(ctx, lists):
+    fn = ctx.client.service.ReportListMessageActivity
+    start_dt = pendulum.parse(ctx.config["start_date"])
+    for lst in lists:
+        for begin_dt, end_dt in gen_intervals(ctx, start_dt):
             response = fn(ListID=lst["ListID"],
-                          StartDate=start_dt,
+                          StartDate=begin_dt,
                           EndDate=end_dt,
                           IncludeTestMessages=True)
-            records = response["ReportListMessageActivityResult"]
-            if not records:
+            actresult = response["ReportListMessageActivityResult"]
+            if not actresult:
                 continue
-            self.write_records(self.transform(ctx, records))
+            messages = transform(actresult["WSMessageActivity"])
+            write_records(IDS.MESSAGES, messages)
+            # message_clicks.sync(ctx, messages)
 
 
-
-all_streams = [
-    SubscribedContacts("subscribed_contacts", []),
-    Messages("messages", ["MsgId"]),
-]
-all_stream_ids = [s.tap_stream_id for s in all_streams]
+def sync_lists(ctx):
+    schemas.load_and_write_schema(IDS.LISTS)
+    response = ctx.client.service.GetContactListCollection()
+    lists = transform(response)
+    # write_records(IDS.LISTS, lists)
+    sync_messages(ctx, lists)
